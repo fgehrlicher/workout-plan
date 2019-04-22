71dd58836729972893d879e0e9b5697ec4b45966
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-04-22 13:25:23 +0200

    Add version to the plan

--- models/plan.go
+++ models/plan.go
@@ -1,7 +1,8 @@
 package models
 
 type Plan struct {
-	ID    string `yaml:"id"`
-	Name  string `yaml:"name"`
-	Units []Unit `yaml:"units"`
+	ID      string `yaml:"id"`
+	Name    string `yaml:"name"`
+	Version string `yaml:"version"`
+	Units   []Unit `yaml:"units"`
 }
