05f8bc1ae4c1d6282f90bddbf5a4c379fe709692
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-04-23 10:27:08 +0200

    Rename iteration to sequence because iteration could be confusing

--- models/exercise.go
+++ models/exercise.go
@@ -1,7 +1,7 @@
 package models
 
 type Exercise struct {
-	Type       string `yaml:"type" json:"type"`
-	Exercise   string `yaml:"exercise" json:"exercise"`
-	Iterations []Iteration `yaml:"iterations" json:"iterations"`
+	Type     string     `yaml:"type" json:"type"`
+	Exercise string     `yaml:"exercise" json:"exercise"`
+	Sequence []Sequence `yaml:"sequence" json:"sequence"`
 }
--- models/sequence.go
+++ models/sequence.go
@@ -1,6 +1,6 @@
 package models
 
-type Iteration struct {
+type Sequence struct {
 	Type    string `yaml:"type" json:"type"`
 	Percent string `yaml:"percent" json:"percent"`
 	Sets    string `yaml:"sets" json:"sets"`
