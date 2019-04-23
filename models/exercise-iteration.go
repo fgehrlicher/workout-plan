25b83f070bfb7186a01406819ac2bf8ab4f9bad3
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-04-23 11:51:15 +0200

    Extend exercise iteration definiton and add validation

--- models/exercise-iteration.go
+++ models/exercise-iteration.go
@@ -1,8 +1,79 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
+type ExerciseIterationValidator func(*ExerciseIteration) error
+
+var possibleExerciseIterations = map[string]ExerciseIterationValidator{
+	"sets-reps":              ValidateExerciseIterationSetsReps,
+	"sets-reps-weight-range": ValidateExerciseIterationSetsRepsWeightRange,
+}
+
 type ExerciseIteration struct {
-	Type    string `yaml:"type" json:"type"`
-	Percent string `yaml:"percent" json:"percent"`
-	Sets    string `yaml:"sets" json:"sets"`
-	Reps    string `yaml:"reps" json:"reps"`
+	Type      string `yaml:"type" json:"type"`
+	MinWeight string `yaml:"min-weight" json:"min-weight"`
+	MaxWeight string `yaml:"max-weight" json:"max-weight"`
+	Percent   string `yaml:"percent" json:"percent"`
+	Sets      string `yaml:"sets" json:"sets"`
+	Reps      string `yaml:"reps" json:"reps"`
+}
+
+func (exerciseIteration *ExerciseIteration) Validate() error {
+	if exerciseIteration.Type == "" {
+		return errors.New(
+			fmt.Sprintf(
+				"type field is required for sequence elements.\nFull element: %+v",
+				exerciseIteration,
+			),
+		)
+	}
+
+	for possibleExerciseIteration, validator := range possibleExerciseIterations {
+		if possibleExerciseIteration == exerciseIteration.Type {
+			return validator(exerciseIteration)
+		}
+	}
+
+	return errors.New(
+		fmt.Sprintf(
+			"type field `%v` is required for sequence elements.\nFull element: %+v",
+			exerciseIteration.Type,
+			exerciseIteration,
+		),
+	)
+}
+
+func ValidateExerciseIterationSetsReps(exerciseIteration *ExerciseIteration) error {
+	if exerciseIteration.Reps == "" || exerciseIteration.Sets == "" {
+		if exerciseIteration.Type == "" {
+			return errors.New(
+				fmt.Sprintf(
+					"reps and sets must be set if the type is set to sets-reps.\nFull element: %+v",
+					exerciseIteration,
+				),
+			)
+		}
+	}
+	return nil
+}
+
+func ValidateExerciseIterationSetsRepsWeightRange(exerciseIteration *ExerciseIteration) error {
+	if exerciseIteration.Reps == "" ||
+		exerciseIteration.Sets == "" ||
+		exerciseIteration.MaxWeight == "" ||
+		exerciseIteration.MinWeight == "" {
+		if exerciseIteration.Type == "" {
+			return errors.New(
+				fmt.Sprintf(
+					"reps, sets, min-weight and max-weight must be set if the " +
+						"type is set to sets-reps-weight-range.\nFull element: %+v",
+					exerciseIteration,
+				),
+			)
+		}
+	}
+	return nil
 }
