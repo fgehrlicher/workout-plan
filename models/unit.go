cd6ce2f6909a53bec03079ba5e87b87e003c298c
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-04-23 16:10:01 +0200

    Add unit validation

--- models/unit.go
+++ models/unit.go
@@ -1,6 +1,31 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Unit struct {
 	Name      string `yaml:"name" json:"name"`
 	Exercises []Exercise `yaml:"exercises" json:"exercises"`
+}
+
+func (unit *Unit) Validate() error {
+	if unit.Name == "" {
+		return errors.New(
+			fmt.Sprintf(
+				"the name of units musn´t be empty.\nFull element: %+v",
+				unit,
+			),
+		)
+	}
+
+	for _, exercise := range unit.Exercises {
+		err := exercise.Validate()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
