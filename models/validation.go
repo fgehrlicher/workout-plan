bacb2c1c123d542282440d895c5c7de1060f3077
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-04-23 17:21:15 +0200

    Add generic `GetTypeField` function to retrieve the `type` field of any struct

--- models/validation.go
+++ models/validation.go
@@ -6,31 +6,52 @@ import (
 	"reflect"
 )
 
+func GetTypeField(element interface{}) (string, error) {
+	var elementValue = reflect.ValueOf(element)
+	kind := elementValue.Kind()
+	if kind == reflect.Ptr {
+		elementValue = reflect.Indirect(elementValue)
+		kind = elementValue.Kind()
+	}
+	if kind != reflect.Struct {
+		return "", errors.New(
+			fmt.Sprintf("element is no struct. Got %T, value: %v",
+				element,
+				element,
+			),
+		)
+	}
+
+	return elementValue.FieldByName("Type").String(), nil
+}
+
 func TypeNotAllowedError(element interface{}) error {
-	name := reflect.TypeOf(element).Name()
-	elementValue := reflect.ValueOf(element)
-	elementType := elementValue.MapIndex(reflect.ValueOf("Type")).Interface().(string)
+	typeField, err := GetTypeField(element)
+	if err != nil {
+		return err
+	}
 
 	return errors.New(
 		fmt.Sprintf(
 			"type field `%v` is not allowed for %T.\nFull element: %+v",
-			elementType,
-			name,
+			typeField,
+			reflect.ValueOf(element).Type().Name(),
 			element,
 		),
 	)
 }
 
 func TypeNotEmptyValidator(element interface{}) error {
-	name := reflect.TypeOf(element).Name()
-	elementValue := reflect.ValueOf(element)
-	elementType := elementValue.MapIndex(reflect.ValueOf("Type")).Interface().(string)
+	typeField, err := GetTypeField(element)
+	if err != nil {
+		return err
+	}
 
-	if elementType == "" {
+	if typeField == "" {
 		return errors.New(
 			fmt.Sprintf(
 				"type field musnt be empty for `%v`.\nFull element: %+v",
-				name,
+				reflect.ValueOf(element).Type().Name(),
 				element,
 			),
 		)
