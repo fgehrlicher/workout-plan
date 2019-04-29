49e436a02113a7cb65f536392baeb0e16ea2dd71
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-04-29 10:17:16 +0200

    Extend the validator with database connection checking

--- cmd/validator/validator.go
+++ cmd/validator/validator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"workout-plan/config"
+	"workout-plan/db"
 	"workout-plan/plan"
 
 	log "github.com/sirupsen/logrus"
@@ -19,6 +20,14 @@ func main() {
 
 	err = plan.InitializePlans(conf.Plans.Directory)
 	handleError(err)
+
+	_, err = db.GetDatabase(
+		conf.Database.Host,
+		conf.Database.Port,
+		conf.Database.User,
+		conf.Database.Password,
+		conf.Database.Database,
+	)
 
 	log.Info("Configuration Valid!")
 }
