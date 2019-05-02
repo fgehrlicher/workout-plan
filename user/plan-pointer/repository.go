d654619466ecef173a04ed579f88ecd0fd661c5a
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-05-02 11:58:42 +0200

    Add basic insert method to the plan pointer repository

--- user/plan-pointer/repository.go
+++ user/plan-pointer/repository.go
@@ -1,6 +1,9 @@
 package plan_pointer
 
 import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -14,4 +17,17 @@ func NewPlanPointerRepository(database *mongo.Database) *PlanPointerRepository {
 
 type PlanPointerRepository struct {
 	collection *mongo.Collection
+}
+
+func (planPointerRepository *PlanPointerRepository) Insert(pointer *PlanPointer) error {
+	planPointerBson := bson.D{
+		{"plan_id", pointer.PlanId},
+		{"position", bson.D{
+			{"unit_id",pointer.Position.Unit.Id},
+			{"exercise_key",pointer.Position.ExercisesKey},
+		}},
+	}
+
+	_, err := planPointerRepository.collection.InsertOne(context.Background(), planPointerBson)
+	return err
 }
