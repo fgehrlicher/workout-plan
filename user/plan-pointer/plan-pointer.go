e52ddde71a0171da07262e8aa0addf460d13a52e
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-05-02 09:35:11 +0200

    add plan pointer constructor

--- user/plan-pointer/plan-pointer.go
+++ user/plan-pointer/plan-pointer.go
@@ -4,6 +4,32 @@ import (
 	"workout-plan/plan"
 )
 
+var plans *plan.Plans
+
+func initializePlans() {
+	if plans == nil {
+		plans = plan.GetPlansInstance()
+	}
+}
+
+func CreatePlanPointer(planId string) (*PlanPointer, error) {
+	initializePlans()
+
+	retrievedPlan, err := plans.Get(planId)
+	if err != nil {
+		return nil, err
+	}
+
+	planPointer := &PlanPointer{
+		PlanId: planId,
+	}
+
+	planPointer.Position.Unit = &retrievedPlan.Units[0]
+	planPointer.Position.ExercisesKey = 0
+
+	return planPointer, nil
+}
+
 type PlanPointer struct {
 	PlanId   string
 	Position struct {
