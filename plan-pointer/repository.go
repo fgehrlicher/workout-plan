c29dec678d555e107657658321302e33dfe01f15
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-05-03 09:27:13 +0200

    Add `init` method to the plan pointer repository to initialize indices

--- plan-pointer/repository.go
+++ plan-pointer/repository.go
@@ -2,21 +2,47 @@ package plan_pointer
 
 import (
 	"context"
+	"fmt"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/bsonx"
 )
 
 const collectionName = "planpointers"
 
-func NewPlanPointerRepository(database *mongo.Database) *PlanPointerRepository {
-	return &PlanPointerRepository{
+func NewPlanPointerRepository(database *mongo.Database) (*PlanPointerRepository, error) {
+	repository := &PlanPointerRepository{
 		collection: database.Collection(collectionName),
 	}
+
+	return repository, repository.init()
 }
 
 type PlanPointerRepository struct {
 	collection *mongo.Collection
+}
+
+func (planPointerRepository *PlanPointerRepository) init() error {
+	indexView := planPointerRepository.collection.Indexes()
+
+	planPointersIndex := mongo.IndexModel{
+		Keys: bsonx.Doc{
+			{"plan_id", bsonx.Int32(1)},
+			{"plan_version", bsonx.Int32(1)},
+			{"user_id", bsonx.Int32(1)},
+		},
+		Options: options.Index().SetName("plan-version-user"),
+	}
+
+	name, err := indexView.CreateOne(
+		context.Background(),
+		planPointersIndex,
+	)
+
+	fmt.Println(name)
+	return err
 }
 
 func (planPointerRepository *PlanPointerRepository) Insert(pointer *PlanPointer) (*mongo.InsertOneResult, error) {
