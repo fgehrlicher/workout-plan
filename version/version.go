69a2acc5060211849d04382d1670e66c876935e3
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-05-06 18:55:08 +0200

    Refactor version comparison

--- version/version.go
+++ version/version.go
@@ -7,42 +7,6 @@ import (
 	"strings"
 )
 
-func IsGreater(baseVersion string, comparatorVersion string) (bool, error) {
-	baseVersionStruct, err := NewVersion(baseVersion)
-	if err != nil {
-		return false, err
-	}
-
-	comparatorVersionStrict, err := NewVersion(comparatorVersion)
-	if err != nil {
-		return false, err
-	}
-
-	MajorBigger := comparatorVersionStrict.Major > baseVersionStruct.Major
-	MajorEqual := comparatorVersionStrict.Major == baseVersionStruct.Major
-
-	MinorBigger := comparatorVersionStrict.Minor > baseVersionStruct.Minor
-	MinorEqual := comparatorVersionStrict.Minor == baseVersionStruct.Minor
-
-	PatchBigger := comparatorVersionStrict.Patch > baseVersionStruct.Patch
-	PatchEqual := comparatorVersionStrict.Patch == baseVersionStruct.Patch
-
-	if MajorBigger || (MajorEqual && MinorBigger) || (MajorEqual && MinorEqual && PatchBigger) {
-		return true, nil
-	}
-
-	if MajorEqual && MinorEqual && PatchEqual {
-		return false, errors.New(
-			fmt.Sprintf(
-				"can´t compare. versions are identical: %v",
-				baseVersion,
-			),
-		)
-	}
-
-	return false, nil
-}
-
 func NewVersion(rawVersion string) (Version, error) {
 	version := Version{}
 
@@ -81,4 +45,36 @@ type Version struct {
 	Major int
 	Minor int
 	Patch int
+}
+
+func IsGreater(baseVersion string, comparatorVersion string) (bool, error) {
+	baseVersionStruct, err := NewVersion(baseVersion)
+	if err != nil {
+		return false, err
+	}
+
+	comparatorVersionStrict, err := NewVersion(comparatorVersion)
+	if err != nil {
+		return false, err
+	}
+
+	MajorBigger := comparatorVersionStrict.Major > baseVersionStruct.Major
+	MajorEqual := comparatorVersionStrict.Major == baseVersionStruct.Major
+
+	MinorBigger := comparatorVersionStrict.Minor > baseVersionStruct.Minor
+	MinorEqual := comparatorVersionStrict.Minor == baseVersionStruct.Minor
+
+	PatchBigger := comparatorVersionStrict.Patch > baseVersionStruct.Patch
+	PatchEqual := comparatorVersionStrict.Patch == baseVersionStruct.Patch
+
+	if MajorEqual && MinorEqual && PatchEqual {
+		return false, errors.New(
+			fmt.Sprintf(
+				"can´t compare. versions are identical: %v",
+				baseVersion,
+			),
+		)
+	}
+
+	return MajorBigger || (MajorEqual && MinorBigger) || (MajorEqual && MinorEqual && PatchBigger), nil
 }
