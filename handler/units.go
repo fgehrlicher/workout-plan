cecbb673dc8fcd7c83562445ebd2a09d6f418ae2
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-05-10 09:27:29 +0200

    Add the `FinishCurrentUnit` http endpoint

--- handler/units.go
+++ handler/units.go
@@ -11,6 +11,11 @@ import (
 	"workout-plan/plan-pointer"
 )
 
+const (
+	PlanFinishedState = "PLAN_FINISHED"
+	PlanOkState       = "OK"
+)
+
 func GetCurrentUnit(response http.ResponseWriter, request *http.Request) {
 	queryParameter := request.URL.Query()
 	userId := queryParameter.Get("user")
@@ -57,5 +62,59 @@ func GetCurrentUnit(response http.ResponseWriter, request *http.Request) {
 	err = json.NewEncoder(response).Encode(currentUnit)
 	if err != nil {
 		internalServerErrorHandler(response, request, err)
+	}
+}
+
+func FinishCurrentUnit(response http.ResponseWriter, request *http.Request) {
+	queryParameter := request.URL.Query()
+	userId := queryParameter.Get("user")
+	planId := mux.Vars(request)["planId"]
+
+	plans := plan.GetPlansInstance()
+	planPointerRepository, err := NewPlanPointerRepository()
+	if err != nil {
+		internalServerErrorHandler(response, request, err)
+		return
+	}
+
+	planPointer, err := planPointerRepository.GetByPlan(userId, planId)
+	if err != nil {
+		if err == plan_pointer.NoPlanFoundError {
+			notFoundErrorHandler(response, request, err)
+		} else {
+			internalServerErrorHandler(response, request, err)
+		}
+		return
+	}
+
+	userPlan, err := plans.Get(planPointer.PlanId, planPointer.PlanVersion)
+	if err != nil {
+		internalServerErrorHandler(response, request, err)
+	}
+
+	if hasPlanUnitsLeft(planPointer, userPlan) {
+		planPointer.Position.Unit ++
+		err = planPointerRepository.Update(planPointer)
+		if err != nil {
+			internalServerErrorHandler(response, request, err)
+			return
+		}
+
+		err = WriteStateMessage(response, "unit finished", PlanOkState)
+		if err != nil {
+			internalServerErrorHandler(response, request, err)
+			return
+		}
+	} else {
+		err := planPointerRepository.Delete(planPointer)
+		if err != nil {
+			internalServerErrorHandler(response, request, err)
+			return
+		}
+		err = WriteStateMessage(response, "plan finished", PlanFinishedState)
+		if err != nil {
+			internalServerErrorHandler(response, request, err)
+			return
+		}
 	}
 }
