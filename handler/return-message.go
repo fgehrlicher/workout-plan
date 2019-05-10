d79648af404f43761486b355b5730c2ef217b74e
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-05-10 08:43:12 +0200

    Extend the return message with an optional state

--- handler/return-message.go
+++ handler/return-message.go
@@ -7,11 +7,20 @@ import (
 
 type ReturnMessage struct {
 	Message string `json:"message"`
-	State   string `json:"state"`
+	State   string `json:"state,omitempty"`
 }
 
 func WriteMessage(response http.ResponseWriter, message string) error {
+	return writeMessage(response, message, "")
+}
+
+func WriteStateMessage(response http.ResponseWriter, message, state string) error {
+	return writeMessage(response, state, message)
+}
+
+func writeMessage(response http.ResponseWriter, message, state string) error {
 	return json.NewEncoder(response).Encode(ReturnMessage{
 		Message: message,
+		State:   state,
 	})
 }
