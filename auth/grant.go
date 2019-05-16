e0d6d26f8e9cf657efeb9894286499c5fff1c326
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-05-16 10:40:35 +0200

    Add grant methods to check if a resource is authorized

--- auth/grant.go
+++ auth/grant.go
@@ -1,9 +1,27 @@
 package auth
 
+const PlanGrantType = "plan"
+
 type Grant struct {
 	UserName string
-	Access []struct{
+	Access   []struct {
 		Name string `json:"name"`
 		Type string `json:"type"`
 	} `json:"access"`
+}
+
+func (grant *Grant) IsAuthorizedForPlan(name string) bool {
+	return grant.IsAuthorized(PlanGrantType, name)
+}
+
+func (grant *Grant) IsAuthorized(accessType string, name string) bool {
+	for _, access := range grant.Access {
+		if access.Type == accessType {
+			if access.Name == name {
+				return true
+			}
+		}
+	}
+
+	return false
 }
