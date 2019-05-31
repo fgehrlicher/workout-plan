98bedb2897331d87501adaa04766c02f468fff43
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-05-31 15:59:51 +0200

    Add the access control allow origin header

--- handler/util.go
+++ handler/util.go
@@ -57,6 +57,7 @@ func hasPlanUnitsLeft(pointer plan.Pointer, userPlan *plan.Plan) bool {
 func HeaderMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
 		responseWriter.Header().Set("Content-Type", "application/json")
+		responseWriter.Header().Set("Access-Control-Allow-Origin", "*")
 		next.ServeHTTP(responseWriter, request)
 	})
 }
