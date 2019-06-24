9a8f08ecefbc67719b823d7ec0e4a171bdabb46e
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-06-24 10:54:17 +0200

    Add the 'RetrieveStats' method to retrieve stats from a given plan and plan pointer

--- stats/statistics.go
+++ stats/statistics.go
@@ -2,14 +2,58 @@ package stats
 
 import (
 	"time"
+
+	"workout-plan/plan"
 )
 
 type Stats struct {
-	TotalUnitCount     int `json:"total_unit_count"`
-	UnitsDone          int `json:"units_done"`
-	TotalExerciseCount int `json:"total_exercise_count"`
-	ExercisesUsed      int `json:"exercises_used"`
-	PlanStarted        time.Time `json:"plan_started"`
-	LastWorkout        time.Time `json:"last_workout"`
-	Variables map[string]string `json:"variables"`
+	TotalUnitCount     int            `json:"total_unit_count"`
+	UnitsDone          int            `json:"units_done"`
+	TotalExerciseCount int            `json:"total_exercise_count"`
+	ExercisesUsed      int            `json:"exercises_used"`
+	PlanStarted        string         `json:"plan_started"`
+	LastWorkout        string         `json:"last_workout"`
+	Variables          map[string]int `json:"variables,omitempty"`
+}
+
+func RetrieveStats(plan plan.Plan, pointer plan.Pointer) (Stats, error) {
+	currentUnit := pointer.Position.Unit - 1
+	stats := Stats{
+		TotalUnitCount: len(plan.Units),
+		UnitsDone:      currentUnit,
+		Variables:      pointer.Data,
+		PlanStarted:    pointer.Started.Format(time.RFC1123),
+	}
+
+	if !pointer.Moved.IsZero() {
+		stats.LastWorkout = pointer.Moved.Format(time.RFC1123)
+	}
+
+	exerciseMap := make(map[string]bool, 0)
+
+	for unitIndex := 0; unitIndex < currentUnit; unitIndex ++ {
+		currentUnit := plan.Units[unitIndex]
+		for _, exercise := range currentUnit.Exercises {
+			exerciseId := exercise.Definition.Id
+			if !exerciseMap[exerciseId] {
+				exerciseMap[exerciseId] = true
+			}
+		}
+	}
+
+	stats.ExercisesUsed = len(exerciseMap)
+
+	for unitIndex := currentUnit; unitIndex < stats.TotalUnitCount; unitIndex ++ {
+		currentUnit := plan.Units[unitIndex]
+		for _, exercise := range currentUnit.Exercises {
+			exerciseId := exercise.Definition.Id
+			if !exerciseMap[exerciseId] {
+				exerciseMap[exerciseId] = true
+			}
+		}
+	}
+
+	stats.TotalExerciseCount = len(exerciseMap)
+
+	return stats, nil
 }
