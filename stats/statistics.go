96163da05923e7ace2724664566dc0c2dee72c7e
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-06-24 08:39:09 +0200

    Add the 'Stats' struct

--- stats/statistics.go
+++ stats/statistics.go
@@ -1,1 +1,14 @@
 package stats
+
+import (
+	"time"
+)
+
+type Stats struct {
+	TotalUnitCount     int `json:"total_unit_count"`
+	UnitsDone          int `json:"units_done"`
+	TotalExerciseCount int `json:"total_exercise_count"`
+	ExercisesUsed      int `json:"exercises_used"`
+	PlanStarted        time.Time `json:"plan_started"`
+	LastWorkout        time.Time `json:"last_workout"`
+}
