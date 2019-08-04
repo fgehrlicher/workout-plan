5060143b1cd59f83dbe412941ab034e09d8c49f8
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2019-08-04 21:25:12 +0200

    Move all media stuff into the media struct

--- plan/exercise-definition.go
+++ plan/exercise-definition.go
@@ -8,12 +8,14 @@ type ExerciseDefinition struct {
 	Id          string `yaml:"id" json:"id"`
 	Name        string `yaml:"name" json:"name"`
 	Description string `yaml:"description" json:"description"`
-	Video       struct {
-		Id string `yaml:"id" json:"id"`
-	} `yaml:"video" json:"video"`
-	Image struct {
-		Url string `yaml:"url" json:"url"`
-	} `yaml:"video" json:"video"`
+	Media       struct {
+		Video struct {
+			Id string `yaml:"id" json:"id"`
+		} `yaml:"video" json:"video"`
+		Image struct {
+			Url string `yaml:"url" json:"url"`
+		} `yaml:"video" json:"video"`
+	} `yaml:"media" json:"media"`
 }
 
 func (exerciseDefinition *ExerciseDefinition) Validate() error {
